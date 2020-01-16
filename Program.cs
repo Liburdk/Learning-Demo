f6044ddeca9d6c89ad630969d568a5b644016289
Kaley Liburd <Liburd.k@yahoo.com>
2020-01-16 18:52:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is ISM 4300!");
+        }
+    }
+}
